abc891fb076d2221904b0e2855090d372a80cb6e
quyencao <boy20195@gmail.com>
2019-10-14 17:31:10 +0700

    new commit

---
+++ graphql-serverless-codebuild-buildspec/create_serverless_yaml.py
@@ -0,0 +1,111 @@
+import os
+import yaml
+from jinja2 import Template
+from six import text_type as _text_type
+
+def _create_serverless_yaml(args):
+    with open(args.inputPath, "r") as c:
+        doc = yaml.load(c.read(), Loader=yaml.FullLoader)
+
+    runtime = doc.get("runtime")
+    tables = doc.get("tables")
+
+    if runtime is None:
+        runtime = "nodejs10.x"
+    if tables is None:
+        tables = []
+
+    serverless_template = Template("""
+service: {{ APP_ID }}
+provider:
+  name: aws
+  runtime: {{ RUNTIME }}
+  {% if TABLES|length > 0 %}
+  iamRoleStatements: # permissions for all of your functions can be set here
+    - Effect: Allow
+      Action: # Gives permission to DynamoDB tables in a specific region
+        - dynamodb:DescribeTable
+        - dynamodb:Query
+        - dynamodb:Scan
+        - dynamodb:GetItem
+        - dynamodb:PutItem
+        - dynamodb:UpdateItem
+        - dynamodb:DeleteItem
+      Resource:
+        {% for table in TABLES %}
+        - "arn:aws:dynamodb:us-east-1:*:table/{{ [APP_ID, table]|join("_") }}"
+        - "arn:aws:dynamodb:us-east-1:*:table/{{ [APP_ID, table]|join("_") }}/*"
+        {% endfor %}
+  {% endif %}
+functions:
+  graphql:
+    # this is formatted as <FILENAME>.<HANDLER>
+    handler: graphqlServer.graphqlHandler
+    events:
+      - http:
+          path: graphql
+          method: post
+          cors: true
+      - http:
+          path: graphql
+          method: get
+          cors: true
+{% if TABLES|length > 0 %}
+resources:
+  Resources:
+    {% for table in TABLES %}
+    {{ table }}: 
+      Type: AWS::DynamoDB::Table
+      Properties:
+        TableName: {{ [APP_ID, table]|join("_") }}
+        AttributeDefinitions:
+          - AttributeName: id
+            AttributeType: S
+        KeySchema:
+          - AttributeName: id
+            KeyType: HASH
+        ProvisionedThroughput:
+          ReadCapacityUnits: 1
+          WriteCapacityUnits: 1
+    {% endfor %}
+{% endif %}
+    """)
+    serverless_text = serverless_template.render({ "APP_ID": args.appId, "TABLES": tables, "RUNTIME": runtime })
+
+    with open(args.outputPath, mode="w") as f:
+        f.write(serverless_text)
+
+def _get_parser():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Create serverless yaml file")
+
+    parser.add_argument(
+        '--appId', '-a',
+        type=_text_type,
+        help="App ID"
+    )
+
+    parser.add_argument(
+        '--inputPath', '-i',
+        type=_text_type,
+        help="Client config input file",
+        default="config.yaml"
+    )
+
+    parser.add_argument(
+        "--outputPath", "-o",
+        type=_text_type,
+        help="Serverless config output file",
+        default="serverless.yaml"
+    )
+
+    return parser
+
+def _main():
+    parser = _get_parser()
+    args = parser.parse_args()
+    _create_serverless_yaml(args)
+
+if __name__ == "__main__":
+    _main()
