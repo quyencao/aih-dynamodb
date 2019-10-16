06a94be04d248a2efe3742038eca6546605372fe
quyencao <boy20195@gmail.com>
2019-10-16 09:13:29 +0700

    add graphql playground

--- serverless-node-graphql/graphqlServer.js
+++ serverless-node-graphql/graphqlServer.js
@@ -2,6 +2,19 @@ const { ApolloServer } = require("apollo-server-lambda");
 const resolvers = require("./resolvers");
 const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+	typeDefs, 
+	resolvers,
+	playground: {
+		settings: {
+			'editor.theme': 'light',
+		},
+		tabs: [
+			{
+				endpoint: "/dev/graphql"
+			}
+		]
+	}
+});
 
 exports.graphqlHandler = server.createHandler();
